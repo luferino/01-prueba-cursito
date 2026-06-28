1770d41f396c03a1f549c9b178d88a4d0a2e80c7
Luis Benegas <luisbene12@gmail.com>
2026-06-28 19:02:07 -0300

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig, envField } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";  
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
   env: {
     schema: {
       CONTENT_ISLAND_SECRET_TOKEN: envField.string({
@@ -16,6 +17,9 @@ export default defineConfig({
       }),
     },
   },
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     plugins: /**@type {any} */ ([tailwindcss()]),
   },
