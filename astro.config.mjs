5d68c08bd28bebbc347c33fc632f83e494aa6099
Luis Benegas <luisbene12@gmail.com>
2026-06-29 22:36:11 -0300

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 import react from '@astrojs/react';
-import tailwindcss from "@tailwindcss/vite";  
+import tailwindcss from "@tailwindcss/vite";
 import node from '@astrojs/node';
 
 // https://astro.build/config
@@ -9,11 +9,28 @@ export default defineConfig({
   integrations: [react()],
   env: {
     schema: {
-      CONTENT_ISLAND_SECRET_TOKEN: envField.string({
-        context: "server",
-        access: "secret",
+      RESEND_API_KEY: envField.string({
+        context: 'server',
+        access: 'secret',
         optional: false,
-        default: "INFORM_VALID_TOKEN",
+        default: 'INFORM_VALID_TOKEN',
+      }), FROM_EMAIL: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_EMAIL',
+      }),
+      TO_EMAIL: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_EMAIL',
+      }),
+      CONTENT_ISLAND_SECRET_TOKEN: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_TOKEN',
       }),
     },
   },
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,11 @@
 import { defineAction } from "astro:actions";
 import { addLike, getLikes } from "./repository";
 import type { LikesResponse } from "./model";
+import { Resend } from 'resend';
+import { RESEND_API_KEY, FROM_EMAIL, TO_EMAIL } from 'astro:env/server';
+import { z } from 'astro:schema';
+
+const resend = new Resend(RESEND_API_KEY);
 
 export const server = {
     addLike: defineAction<LikesResponse>({
@@ -13,4 +18,25 @@ export const server = {
             return { likes: await getLikes(slug) };
         }, 
     }),
+    sendSubscription: defineAction({
+        accept: 'form',
+        input: z.object({
+            email: z.string().email('invalid email'),
+        }),
+        handler: async input => {
+            try {
+                const {email} = input;
+                await resend.emails.send({
+                    from: FROM_EMAIL,
+                    to: TO_EMAIL,
+                    subject: 'hello world',
+                    html:`<p>Congrats on sending your <strong>${email}</strong>!</p>`, 
+                })
+                return {succes: true, message: 'E-mail sent successfully ✅'}
+            } catch (error) {
+                return { success: false, message: 'There was an error sending the e-mail ❌' };
+            }
+        }
+    }),
+
 };
